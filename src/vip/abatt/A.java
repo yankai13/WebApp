40bd5b354b7b219bf9a38129883bc2afd2d30433
yankai13 <yankai@lunaon.com>
2020-03-23 14:58:50 +0800

    修改baseurl

---
+++ src/vip/abatt/A.java
@@ -0,0 +1,10 @@
+package vip.abatt;
+
+/**
+ * Author:YANKAI_1101
+ * Date:2020/3/23
+ * Desc：
+ **/
+public class A {
+    // 修改baseUrl
+}
---
+++ src/vip/abatt/Main.java
@@ -0,0 +1,8 @@
+package vip.abatt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// bbbbb
+    }
+}
